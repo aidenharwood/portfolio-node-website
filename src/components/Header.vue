20f243b10cdfc22808fc6e7f1c1c6917d237e14e
carbage <carbage@github.com>
2025-08-04 20:37:39 +0100

    Configure build pipeline

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,28 @@
-<script setup>
-import { ref } from 'vue'
-
-const parentMessage = ref('Parent')
-const links = ref([
-  { icon: 'pi-info-circle', route: '/', name: 'About' },
-  { icon: 'pi-folder-open', route: '/projects', name: 'Projects' },
-  { icon: 'pi-briefcase', route: '/experience', name: 'Experience' }
-])
-</script>
-
 <template>
   <section class="flex py-4 px-4 mx-12 content-center justify-center
     fixed bottom-4 left-0 right-0
     md:fixed md:top-0  md:bottom-auto md:translate-x-0
   ">
-    <section :class="`flex justify-around px-4 py-2 shadow-xl justify-items-center content-center text-center space-x-4 bg-slate-800/80 rounded-xl`">
+    <section
+      :class="`flex justify-around px-4 py-2 shadow-xl justify-items-center content-center text-center space-x-4 bg-slate-800/80 rounded-xl`">
       <section v-for="link in links" className="flex-1 min-w-[80px] flex flex-col items-center group">
         <router-link :to="link.route" class="hover:text-slate-400 transition-colors">
           <i :class="`pi ${link.icon}`"></i>
         </router-link>
         <section class="hidden md:block">
           {{ link.name }}
-          <section
-            className="hidden md:block h-0.5 bg-slate-400 w-0 group-hover:w-1/2 transition-all mx-auto">
+          <section className="hidden md:block h-0.5 bg-slate-400 w-0 group-hover:w-1/2 transition-all mx-auto">
           </section>
         </section>
       </section>
     </section>
   </section>
 </template>
+<script setup lang="ts">
+import { ref } from 'vue'
+const links = ref([
+  { icon: 'pi-info-circle', route: '/', name: 'About' },
+  { icon: 'pi-folder-open', route: '/projects', name: 'Projects' },
+  { icon: 'pi-briefcase', route: '/experience', name: 'Experience' }
+])
+</script>
