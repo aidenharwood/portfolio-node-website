438d9ce465e5d3876e8b7edbf95d01109bfa5a77
Aiden Harwood <carbage@github.com>
2025-09-06 13:55:09 +0100

    Only show Argo panel on homepage

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,10 @@ import ArgoCDCard from '@/components/ArgoCDCard.vue'
 
 <template>
   <main
-    class="flex flex-col min-h-screen bg-gradient-to-br from-slate-900 to-slate-800 text-slate-200 px-4 lg:px-10 pt-10 lg:pt-30 pb-30 lg:pb-10"
-  >
+    class="flex flex-col min-h-screen bg-gradient-to-br from-slate-900 to-slate-800 text-slate-200 px-4 lg:px-10 pt-10 lg:pt-30 pb-30 lg:pb-10">
     <Navigation />
-    <section class="lg:flex w-full lg:space-x-10 lg:space-y-0 space-y-10 justify-items-center justify-center items-start">
+    <section
+      class="lg:flex w-full lg:space-x-10 lg:space-y-0 space-y-10 justify-items-center justify-center items-start">
       <InfoCard />
       <section class="flex flex-col w-full max-w-full items-start lg:px-10 px-0 flex-1 min-w-0 min-h-0
         [&_*::-webkit-scrollbar]:w-0.25
@@ -21,7 +21,9 @@ import ArgoCDCard from '@/components/ArgoCDCard.vue'
          ">
         <router-view class="max-w-full w-full min-w-0 flex-1" />
       </section>
-      <ArgoCDCard />
+      <v-if v-if="$route.path === '/'">
+        <ArgoCDCard />
+      </v-if>
     </section>
   </main>
 </template>
